ccf9031a3eede97fbc2ab9379727f4e9dde4ae90
Katherine-cym <yimeng_cao@outlook.com>
2024-12-02 07:38:47 +0000

    1

--- hw1_ans.py
+++ hw1_ans.py
@@ -1,13 +1,11 @@
+#1
 from flask import Flask
 from flask import render_template, request
 
 app = Flask(__name__)
 
-@app.route("/",methods = ['GET','POST'])
-def index():
-    return(render_template('index.html'))
 
-@app.route("/main",methods = ['GET','POST'])
+@app.route("/",methods = ['GET','POST'])
 def main():
     name = request.form.get("q")
 
